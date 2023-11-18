63c9715e97192b48d8c2031c183e384da229bf25
nikBaben <faymen44@mail.ru>
2023-11-18 03:28:53 +0300

    правка программ и заполенение отчета

--- 5_2task.c
+++ 5_2task.c
@@ -4,58 +4,14 @@
 #include <math.h>
 #include "4task.h" //Импортируем нашу библиотеку для работы с матрицами
 
-//Функция проверки корректности строки по условию задачи(хотя бы один элемент строки не равен другим)
-int string_correctness(int **matrix, int row,int column,int index){
-    
-        int flag = 0;                      //Подсчет кол-ва одинаковых элементов в строке
-        int element = matrix[index][0];    //Первый элемент полученного индекса строки 
 
-        for(int j=0; j<column; j++){
-            if(abs(element) == abs(matrix[index][j])){
-                flag +=1;
-            } 
-        }
-        if (flag == column) { 
-            //В случае, если все элементы строки равны между собой возвращаем 0(Проверка на корректность не прошла)
-            return 0;
-        }
-        //В случае, если корректность строки пройдена.
-        return 1;
-}
+//Прототип функции проверки корректности строки по условию задачи(хотя бы один элемент строки не равен другим)
+int string_correctness(int **matrix, int row,int column,int index);
+//Прототип функция для проверки на диагонали 
+int is_on_diagonal(int **matrix, int size,int *element);
+//Прототип функции изменения знака максимального по модолю элемента в строке
+void swap_sign_of_element(int **matrix,int row,int column);
 
-
-//Функция изменения знака максимального по модолю элемента в строке
-void swap_sign_of_element(int **matrix,int row,int column){
-    int max_char = INT_MIN; //Число для сравнения с элементами матрицы
-    int *p;                 //Указатель на максимальный элемент строки.
-    int flag = 0;           //Флаг для корректности строки
-    int stop = 0;           //Флаг для остановки изменения знака элемента строки 
-
-    for(int i = 0; i<row; i++){
-        flag = string_correctness(matrix,row,column,i); //Вызываем функцию проверки текущей строки на коррекность.
-        if(flag){
-            for(int j =0; j<column; j++)
-            {   
-                //Проверка условия на максимальный элемент строки и элемент не лежит на диагоналях матрицы
-                if((abs(matrix[i][j]) > max_char) && ((i != j) && (j!=row-i-1)))
-                {
-                    max_char =abs(matrix[i][j]);
-                    p = &matrix[i][j]; //Запоминаем адрес максимального элемента 
-                }
-                if((abs(matrix[i][j]) > max_char)){
-                    stop = 1;
-                }
- 
-            }
-            //Изменение знака максимального элемента строки массива
-            if(!stop){
-            *p = *p*(-1);
-            max_char = INT_MIN;
-            }
-            stop = 0;
-        }
-    }
-}
 
 
 int main(){
@@ -84,11 +40,84 @@ int main(){
         printf("\n Ваша матрица\n");
         output_int_matrix(matrix,row,column);             //Вызываем функцию вывода матрицы
         swap_sign_of_element(matrix,row,column);          //Вызываем функцию изменения знака максимального элемента строки матрицы 
-        
+
         printf("Измененная матрица\n");
         output_int_matrix(matrix,row,column);              //Вызываем функцию вывода матрицы
         printf("\n");
     }
 
     return 0;
+}
+
+
+//Функция проверки корректности строки по условию задачи(хотя бы один элемент строки не равен другим)
+int string_correctness(int **matrix, int row,int column,int index){
+    
+        int flag = 0;                      //Подсчет кол-ва одинаковых элементов в строке
+        int element = matrix[index][0];    //Первый элемент полученного индекса строки 
+
+        //Проверка на одинаковость элементов строки
+        for(int j=0; j<column; j++){
+            if(abs(element) == abs(matrix[index][j])){
+                flag +=1;
+            } 
+        }
+        if (flag == column) { 
+            //В случае, если все элементы строки равны между собой возвращаем 0(Проверка на корректность не прошла)
+            return 0;
+        }
+        //В случае, если корректность строки пройдена.
+        return 1;
+}
+
+//Функция для проверки на диагонали 
+int is_on_diagonal(int **matrix, int size,int *element){
+    //Проверка лежит ли элемент на главной диагонали
+    for(int i = 0; i < size;i++){
+        if ( &(matrix[i][i]) == &(*element)){
+            return 1;
+        }
+    }
+    //Проверка лежит ли элемент на побочной диагонали
+    for(int j = 0, i = size-1; j< size && i >= 0 ;--i, ++j){
+        if(&(matrix[j][size- 1 - j]) == &(*element)){
+            return 1;
+        }
+    }
+    return 0; 
+}
+
+//Функция изменения знака максимального по модолю элемента в строке
+void swap_sign_of_element(int **matrix,int row,int column){
+    int max_char = INT_MIN; //Число для сравнения с элементами матрицы
+    int *p;                 //Указатель на максимальный элемент строки.
+    int flag = 0;           //Флаг для корректности строки
+    int stop = 0;           //Флаг для остановки изменения знака элемента строки 
+
+    for(int i = 0; i<row; i++){
+        flag = string_correctness(matrix,row,column,i); //Вызываем функцию проверки текущей строки на коррекность.
+        if(flag){
+            for(int j =0; j<column; j++){   
+                //Проверка условия на максимальный элемент строки
+                if(abs(matrix[i][j]) > max_char){
+                    //Если элемент не лежит на диагоналях матрицы
+                    if(!is_on_diagonal(matrix,row,&matrix[i][j])){
+                        max_char = abs(matrix[i][j]);
+                        p = &matrix[i][j]; //Запоминаем адрес максимального элемента 
+                    }
+                    else{
+                        p = NULL; 
+                        
+                    }
+                }
+            }
+            //Изменение знака максимального элемента строки массива
+            if(p != NULL){
+                *p = *p*(-1);
+            }
+            max_char = INT_MIN;
+            stop = 0;
+        }
+    }
+
 }
